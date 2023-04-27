2f65dab0ce226f76073ce0fa0a054ead58c4b68f
Mark Rasavong <rasavong.mark@gmail.com>
2023-04-27 10:10:08 +0200

    Add: Header component, Desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import reactLogo from './assets/react.svg';
+import Header from './components/Header';
 import viteLogo from '/vite.svg';
 
 function App() {
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<p className="text-2xl">
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<Header />
+			<main className="bg-darkBg flex-grow text-white">hello</main>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/app/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<Provider store={store}>
+		<BrowserRouter>
 			<App />
-		</Provider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
