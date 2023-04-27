2391ad365e49ffafb392a179946910b6ea7e002a
Mark Rasavong <rasavong.mark@gmail.com>
2023-04-27 13:46:43 +0200

    Add: resusable component class, component-shape v2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,12 +61,12 @@ const Header = () => {
 						<input
 							type="text"
 							placeholder="Search..."
-							className="component-shape hidden md:inline-block md:w-3/4 py-2 md:px-4 px-0 md:mr-2 dark:focus:ring-darkModeText"
+							className="componentShape hidden md:inline-block md:w-3/4 py-2 md:px-4 px-0 md:mr-2 dark:focus:ring-darkModeText"
 							style={{ paddingLeft: '36px' }}
 						/>
 						<SearchIcon className="hidden md:inline-block absolute md:w-4 md:left-20 md:ml-7 lg:left-40 lg:ml-0.5 lg:mr-5" />
 					</form>
-					<select name="currency" className={'component-shape px-4 py-2 mr-2 cursor-pointer'}>
+					<select name="currency" className={'componentShape px-4 py-2 mr-2 cursor-pointer'}>
 						{fiatCurrencies.map(({ value, symbol }, index) => (
 							<option
 								value={value}
@@ -82,7 +82,7 @@ const Header = () => {
 					<button
 						type="button"
 						onClick={handleThemeToggle}
-						className="component-shape p-2 focus:outline-none focus:ring-2 focus:ring-darkModeText focus:border-transparent"
+						className="componentShape p-2 focus:ring-darkModeText"
 					>
 						<ThemeToggleIcon />
 					</button>
