75e7e7667fdf1a3336d57f240e9028b9fb498c44
Mark Rasavong <rasavong.mark@gmail.com>
2023-04-27 10:46:13 +0200

    Style: toggle themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import reactLogo from './assets/react.svg';
 import Header from './components/Header';
-import viteLogo from '/vite.svg';
+import { useAppSelector } from './redux/app/hooks';
 
 function App() {
+	const { darkMode } = useAppSelector((state) => state.theme);
+
 	return (
-		<>
+		<body className={`${darkMode && 'dark'} min-h-screen flex flex-col`}>
 			<Header />
-			<main className="bg-darkBg flex-grow text-white">hello</main>
-		</>
+			<main className="dark:bg-darkBg bg-lightModeBgGray flex-grow text-white"></main>
+		</body>
 	);
 }
 
