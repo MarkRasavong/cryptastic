a479d97db230d0c9c5e36d513aac98b9ccf59238
Mark Rasavong <rasavong.mark@gmail.com>
2023-05-17 17:13:46 +0200

    Add: CoinGraph shape render

---
+++ src/components/CoinGraphTitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinGraphTitle = () => {
+	return (
+		<div className="h-full">
+			<h3></h3>
+		</div>
+	);
+};
+
+export default CoinGraphTitle;
--- src/components/CoinGraphs.tsx
+++ src/components/CoinGraphs.tsx
@@ -3,8 +3,14 @@ import CryptoDropdown from './CryptoDropdown';
 
 const CoinGraphs = () => {
 	return (
-		<div className="flex gap-4 ">
+		<div className="flex flex-col">
 			<CryptoDropdown />
+			<div className="flex justify-between">
+				<div className="flex flex-col items-center relative rounded-lg dark:bg-darkNonIntComponentBg bg-lightModeWhite w-[48%] h-80">
+					<div className="flex justify-between items-start w-full"></div>
+				</div>
+				<div className="flex flex-col items-center relative rounded-lg dark:bg-darkNonIntComponentBg bg-lightModeWhite w-[48%] h-80"></div>
+			</div>
 		</div>
 	);
 };
