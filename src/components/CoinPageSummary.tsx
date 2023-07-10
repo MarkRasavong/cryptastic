f35adfb3feb862afaa5415ecb1b3045990cd908c
Mark Rasavong <rasavong.mark@gmail.com>
2023-07-10 19:59:47 +0200

    Add: CoinPageSummary Component

---
+++ src/components/CoinPageSummary.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { CoinPageData } from '../pages/CoinPage';
+import { FaLink } from 'react-icons/fa';
+
+interface CoinPageSummaryProps {
+	profile: CoinPageData;
+}
+
+const CoinPageSummary = ({ profile }: CoinPageSummaryProps) => {
+	return (
+		<div>
+			<div className="bg-lightModeWhite dark:bg-darkNonIntComponentBg dark:text-white rounded-lg px-6 py-12 flex flex-col justify-center text-center items-center">
+				<div className="bg-lightModeBgGray dark:bg-darkIntComponentBg rounded-lg text-center flex justify-center p-4 w-fit">
+					<img src={profile.image.thumb} alt={`${profile.name} logo`} />
+				</div>
+				<div className="text-sm mt-1">
+					<span id="profile_name_cryptoTitle" className="whitespace-nowrap">
+						{profile.name}
+						{profile.name.length > 7 && (
+							<>
+								<br />
+							</>
+						)}
+					</span>
+					<span id="profile_symbol_cryptoTitle" className="whitespace-nowrap">
+						{profile.symbol.length !== 0 && ` (${profile.symbol.toUpperCase()})`}
+					</span>
+				</div>
+			</div>
+			<div className="bg-lightModeWhite dark:bg-darkNonIntComponentBg dark:text-white rounded-lg mt-2 p-4 flex">
+				<a href={profile.links.homepage[0]} target="_blank" rel="noreferrer" className="mr-4">
+					<FaLink />
+				</a>
+				<a href={profile.links.homepage[0]} target="_blank" rel="noreferrer" className="text-xs">
+					{profile.links.homepage[0].slice(8, -1)}
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default CoinPageSummary;
--- src/pages/CoinPage.tsx
+++ src/pages/CoinPage.tsx
@@ -2,11 +2,197 @@ import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../redux/app/hooks';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import CoinPageSummary from '../components/CoinPageSummary';
+
+export interface CoinPageData {
+	id: string;
+	symbol: string;
+	name: string;
+	asset_platform_id: null | string;
+	platforms: {
+		[key: string]: string;
+	};
+	detail_platforms: {
+		[key: string]: {
+			decimal_place: null | number;
+			contract_address: string;
+		};
+	};
+	block_time_in_minutes: number;
+	hashing_algorithm: string;
+	categories: string[];
+	public_notice: null;
+	additional_notices: any[];
+	description: {
+		en: string;
+	};
+	links: {
+		homepage: string[];
+		blockchain_site: string[];
+		official_forum_url: string[];
+		chat_url: string[];
+		announcement_url: string[];
+		twitter_screen_name: string;
+		facebook_username: string;
+		bitcointalk_thread_identifier: number;
+		telegram_channel_identifier: string;
+		subreddit_url: string;
+		repos_url: {
+			github: string[];
+			bitbucket: string[];
+		};
+	};
+	image: {
+		thumb: string;
+		small: string;
+		large: string;
+	};
+	market_data: {
+		current_price: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		total_value_locked: null;
+		mcap_to_tvl_ratio: null;
+		fdv_to_tvl_ratio: null;
+		roi: null;
+		ath: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		ath_change_percentage: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		ath_date: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		atl: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		atl_change_percentage: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		atl_date: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		market_cap: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		market_cap_rank: number;
+		fully_diluted_valuation: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		total_volume: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		high_24h: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		low_24h: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_24h: number;
+		price_change_percentage_24h: number;
+		price_change_percentage_7d: number;
+		price_change_percentage_14d: number;
+		price_change_percentage_30d: number;
+		price_change_percentage_60d: number;
+		price_change_percentage_200d: number;
+		price_change_percentage_1y: number;
+		market_cap_change_24h: number;
+		market_cap_change_percentage_24h: number;
+		price_change_24h_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_percentage_1h_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_percentage_24h_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_percentage_7d_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_percentage_14d_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_percentage_30d_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_percentage_60d_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_percentage_200d_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		price_change_percentage_1y_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		market_cap_change_24h_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		market_cap_change_percentage_24h_in_currency: {
+			eur: number;
+			gbp: number;
+			usd: number;
+		};
+		total_supply: number;
+		max_supply: null;
+		circulating_supply: number;
+		sparkline_7d: {
+			price: number[];
+		};
+		last_updated: string;
+	};
+	last_updated: string;
+}
 
 const CoinPage: React.FC = () => {
 	const { id } = useParams();
 	const currency = useAppSelector((state) => state.currency.value);
-	const [profile, setProfile] = useState<any>();
+	const [profile, setProfile] = useState<CoinPageData>();
 	const [isLoading, setIsLoading] = useState(false);
 
 	const getCoinInfo = async () => {
@@ -30,7 +216,22 @@ const CoinPage: React.FC = () => {
 
 	console.log(profile);
 
-	return <div></div>;
+	return (
+		<section className="max-w-screen-md mx-auto">
+			{hasCoinProfile && (
+				<div className="flex flex-col my-4">
+					<div className="mb-6">
+						<h1>{profile.name} Summary</h1>
+					</div>
+					<div className="flex justify-between">
+						<CoinPageSummary profile={profile} />
+						<div className="bg-lightModeWhite dark:bg-darkNonIntComponentBg dark:text-white h-12 w-12 rounded-lg"></div>
+						<div className="bg-lightModeWhite dark:bg-darkNonIntComponentBg dark:text-white h-12 w-12 rounded-lg"></div>
+					</div>
+				</div>
+			)}
+		</section>
+	);
 };
 
 export default CoinPage;
