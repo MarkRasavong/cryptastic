d856b3bb9fa6c83f48648a9d0716efb4272ec2bf
Mark Rasavong <rasavong.mark@gmail.com>
2023-08-19 20:08:23 -0700

    Style Portfolio page mobile repsonsiveness

--- src/pages/PortolioPage.tsx
+++ src/pages/PortolioPage.tsx
@@ -39,8 +39,8 @@ const PortfolioPage = () => {
 			<div className="flex justify-center w-full my-10">
 				<button className="bg-cryptoGreen px-20 py-3 rounded-lg">Add Asset</button>
 			</div>
-			<section id="portfolio-my-assets">
-				<h1 className="mb-8">My Assets</h1>
+			<section id="portfolio-my-assets" className="w-11/12">
+				<h1 className="mb-8 ml-5">My Assets</h1>
 				{loading ? (
 					<div>Loading...</div>
 				) : (
@@ -54,10 +54,10 @@ const PortfolioPage = () => {
 						return (
 							<div
 								key={profile.id + keyNumber}
-								className={`${idx !== 0 && 'mt-6'} flex justify-between`}
+								className={`${idx !== 0 && 'mt-6'} flex md:justify-between flex-col lg:flex-row`}
 							>
 								<div
-									className="bg-lightModeWhite dark:bg-darkNonIntComponentBg flex flex-col justify-center items-center rounded-lg sm:items-center cursor-pointer px-8 pb-5"
+									className="bg-lightModeWhite dark:bg-darkNonIntComponentBg ml-5 flex flex-col justify-center items-center rounded-lg sm:items-center cursor-pointer lg:px-8 lg:pb-5 py-10"
 									onMouseOver={() => setDisplayTrashIcon(true)}
 									onMouseOut={() => setDisplayTrashIcon(false)}
 								>
@@ -74,7 +74,7 @@ const PortfolioPage = () => {
 												{profile.symbol && ` (${profile.symbol.toUpperCase()})`}
 											</span>
 										</div>
-										<div className="text-[0.5rem] text-center mt-2 h-8 flex justify-center items-center hover:w-16 hover:dark:bg-darkIntComponentBg rounded-lg hover:bg-lightModeBgGray">
+										<div className="text-xs lg:text-[0.5rem] text-center mt-2 h-8 flex justify-center items-center hover:w-16 hover:dark:bg-darkIntComponentBg rounded-lg hover:bg-lightModeBgGray">
 											<div id="asset-last-updated" className="w-16 flex justify-center">
 												{displayTrashIcon ? (
 													<FaTrash size={'0.5rem'} />
@@ -85,19 +85,19 @@ const PortfolioPage = () => {
 										</div>
 									</div>
 								</div>
-								<div className="flex flex-col text-[0.6rem] ml-5">
+								<div className="flex flex-col text-sm lg:text-[0.6rem] ml-5 mt-3 lg:mt-0">
 									<h6 className="mb-2">Market Price:</h6>
-									<div className="flex justify-around bg-lightModeWhite dark:bg-darkNonIntComponentBg rounded-lg">
+									<div className="flex flex-col lg:flex-row justify-around bg-lightModeWhite dark:bg-darkNonIntComponentBg rounded-lg">
 										<div className="flex p-3 items-center">
 											<p>Current price: </p>{' '}
-											<p className="text-cryptoGreen">
+											<p className="text-cryptoGreen ml-2">
 												{setCurrency(currency)}
 												{moneySuffix(roundToNumber(profile.currentPrice, 2))}
 											</p>
 										</div>
 										<div className="flex p-3 items-center">
 											<p>Price change 24h: </p>{' '}
-											<p className="flex items-center">
+											<p className="flex items-center ml-2">
 												{profile.priceChange24h > 0 ? <TickerSymbolUp /> : <TickerSymbolDown />}
 												<span
 													className={`${
@@ -109,9 +109,13 @@ const PortfolioPage = () => {
 											</p>
 										</div>
 										<div className="flex p-3 items-center">
-											<p className="mr-1">Market Cap vs Volume: </p>
+											<p className="mr-2">Market Cap vs Volume: </p>
 											<p className="mr-1 text-cryptoGreen">
-												{isNaN(marketCapToTotalVol) ? '∞' : `${marketCapToTotalVol}%`}
+												{isNaN(marketCapToTotalVol) ? (
+													<span className="text-lg">∞</span>
+												) : (
+													`${marketCapToTotalVol}%`
+												)}
 											</p>
 											<span>
 												<ProgressBar widthPercentage={marketCapToTotalVol} />
@@ -119,15 +123,17 @@ const PortfolioPage = () => {
 										</div>
 										<div className="flex p-3 items-center">
 											<p>Circ Supply vs Max Supply: </p>{' '}
-											<span className="text-cryptoGreen">
-												{profile.maxSupply !== null && profile.circulatingSupply !== null
-													? `${((profile.circulatingSupply * 100) / profile.maxSupply).toFixed(0)}%`
-													: '∞'}
+											<span className="text-cryptoGreen ml-2">
+												{profile.maxSupply !== null && profile.circulatingSupply !== null ? (
+													`${((profile.circulatingSupply * 100) / profile.maxSupply).toFixed(0)}%`
+												) : (
+													<span className="text-lg">∞</span>
+												)}
 											</span>
 										</div>
 									</div>
 									<h6 className="my-2">Your Coin:</h6>
-									<div className="flex justify-around bg-lightModeWhite dark:bg-darkNonIntComponentBg rounded-lg">
+									<div className="flex flex-col lg:flex-row justify-around bg-lightModeWhite dark:bg-darkNonIntComponentBg rounded-lg">
 										<div className="flex p-3 items-center">
 											<p className="mr-1">Coin Amount: </p>{' '}
 											<span className="text-cryptoGreen">{profile.coinAmount}</span>
